fc2bb9a76a3fb15761a6a81cdb1188bc0f1dde0f
Rafael Mascarenhas <rafaelmascarenhas63@gmail.com>
2021-06-02 22:47:49 -0300

    Primeiro commit do projeto

---
+++ Aula11/Ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velociade do carro é ${vel} km/h`);
+if (vel > 60){
+    console.log('Voce ultrapassou o limite de velociade. MULTADO!');
+}
+console.log('Dirija sempre com o cinto de segurança');
---
+++ Aula13/ambiente.js
@@ -0,0 +1,11 @@
+/*var c = 1
+do { 
+    console.log('Tudo bem?');
+    c++
+}while (c <= 10)*/
+
+var c = 1
+while (c <= 6){
+    console.log('Tudo bem?');
+    c++
+}
